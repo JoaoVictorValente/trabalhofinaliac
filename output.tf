2e1ea2ee3ae1f993b063fd2fe4557a1d0b847740
JoaoVictorValente <117855748+JoaoVictorValente@users.noreply.github.com>
2024-07-13 14:19:17 -0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -17,3 +17,14 @@ output "linux_virtual_machine_names" {
 output "linux_virtual_machine_ips" {
   value = [for s in azurerm_linux_virtual_machine.myVM : s.public_ip_address[*]]
 }
+
+# Output SSH Private Key
+output "ssh_private_key" {
+  value     = tls_private_key.ssh_key[0].private_key_pem
+  sensitive = true
+}
+
+# Output Public IP
+output "public_ip" {
+  value = azurerm_public_ip.myPubIP[*].ip_address
+}
