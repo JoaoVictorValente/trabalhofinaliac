a589de246b313882203dbd6c86a0bb11d6027fbe
JoaoVictorValente <117855748+JoaoVictorValente@users.noreply.github.com>
2024-07-13 11:07:21 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -133,9 +133,40 @@ resource "azurerm_linux_virtual_machine" "myVM" {
 
 # Generate Ansible Inventory
 resource "local_file" "hosts_cfg" {
-  content  = templatefile("inventory.tpl", {
+  content = templatefile("inventory.tpl", {
     vms      = azurerm_linux_virtual_machine.myVM
     username = var.username
   })
   filename = "./ansible/inventory.yml"
+}
+
+# Variables
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Local onde o grupo de recursos será criado"
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefixo que será anexado ao nome randômico de grupo de recursos"
+}
+
+variable "username" {
+  type        = string
+  description = "O usuário que será usado para nos conectarmos nas VMs"
+  default     = "azureadmin"
+}
+
+variable "number_resources" {
+  type        = number
+  default     = 1
+  description = "Número de VMs que serão criadas"
+}
+
+variable "admin_password" {
+  description = "Password for the admin user on the virtual machine"
+  type        = string
+  default     = ""
 }
